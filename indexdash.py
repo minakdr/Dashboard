6bd9c43c67a7d26cb06a828d7c60d40daf82710a
minakdr <mina125.ak@gmail.com>
2024-11-13 20:43:22 +0100

    removed my last comment

--- indexdash.py
+++ indexdash.py
@@ -9,7 +9,7 @@ import pandas as pd
 url_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 url_deaths = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
 url_recovered = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
-# this is a comment
+
 confirmed = pd.read_csv(url_confirmed)
 deaths = pd.read_csv(url_deaths)
 recovered = pd.read_csv(url_recovered)
